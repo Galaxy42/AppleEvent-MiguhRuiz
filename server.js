7d950cf85d6001d7233890037d00b0b4a3641145
Miguh Ruiz <miguhruiz@gmail.com>
2016-03-11 16:14:17 +0100

    Defined React as render

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+require('node-jsx').install({harmony: true});
 var express = require('express');
+var reactEngine = require('react-engine');
+
 var app = express();
 var port = process.env.PORT || 3000;
+
+app.set('views', __dirname + 'src/views');
+
+var engine = reactEngine.server.create();
+
+app.engine('.jsx', engine);
+app.set ('view engine', 'jsx');
+app.set('view', reactEngine.expressView);
 
 app.listen(port, function(){
 	console.log("Aplicación en el puerto => " + port);
