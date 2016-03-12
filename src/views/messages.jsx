13a2bf50307cae8905ad13cca0ab29442416aba2
Miguh Ruiz <miguhruiz@gmail.com>
2016-03-12 13:36:59 +0100

    Start doing request to the Twitter API

--- src/views/messages.jsx
+++ src/views/messages.jsx
@@ -1,6 +1,12 @@
 var React = require('react');
+var $ = require('jquery');
+
+var authKeys = require('../../auth.json');
 
 var messages = React.createClass({
+	componentWillMount: function() {
+		$.get('#', function(){});	
+	},
 	render() {
 		return (
 			<h1>{this.props.title}</h1>
