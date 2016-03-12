492cc77586224f11da603f55a80b577cd10cc025
Miguh Ruiz <miguhruiz@gmail.com>
2016-03-12 14:40:54 +0100

    Using 'twitter' NPM module to get data from Twitter

--- src/views/messages.jsx
+++ src/views/messages.jsx
@@ -1,11 +1,24 @@
 var React = require('react');
+var Twitter = require('twitter');
 var $ = require('jquery');
 
 var authKeys = require('../../auth.json');
 
+var Client = new Twitter({
+	consumer_key: authKeys.Twitter.consumerKey,
+	consumer_secret: authKeys.Twitter.consumerSecret,
+	access_token_key: authKeys.Twitter.tokenKey,
+	access_token_secret: authKeys.Twitter.tokenSecret
+});
+
 var messages = React.createClass({
 	componentWillMount: function() {
-		$.get('#', function(){});	
+		Client.get('search/tweets.json', {q: "%23AppleEventMiguhRuiz"}, function(err, tweets, response) {
+			if(err) {
+				console.log(err);
+			}
+			console.log(tweets);
+		});
 	},
 	render() {
 		return (
